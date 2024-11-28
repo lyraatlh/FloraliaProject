a5327f7ffe57b00b243263ecc91c83ace441648c
Lyra Attallah <lyrattallah@gmail.com>
2024-11-28 20:03:58 +0800

    update FloraiaProject

--- app/src/main/java/com/example/floraliaproject/HomeFragment.java
+++ app/src/main/java/com/example/floraliaproject/HomeFragment.java
@@ -1,47 +1,64 @@
 package com.example.floraliaproject;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Random;
 
 public class HomeFragment extends Fragment {
 
-    private RecyclerView rvFlowers;
+    private ImageView imgRecommendation;
+    private TextView tvFlowerName, tvMeaning;
+    private Handler handler;
+    private Runnable imageSwitcher;
     private ArrayList<Flower> flowerList;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        // Ambil RecyclerView
-        rvFlowers = view.findViewById(R.id.rv_flowers);
-        rvFlowers.setHasFixedSize(true);
-
-        // Ambil data bunga
+        imgRecommendation = view.findViewById(R.id.img_recommendation);
+        tvFlowerName = view.findViewById(R.id.tv_flower_name);
+        tvMeaning = view.findViewById(R.id.tv_flower_meaning);
         flowerList = FlowerDetailFragment.getListData();
 
-        // Acak daftar bunga
-        Collections.shuffle(flowerList);
-
-        // Ambil 5 bunga teratas
-        ArrayList<Flower> randomFlowers = new ArrayList<>(flowerList.subList(0, 5));
-
-        // Tampilkan di RecyclerView
-        ListFlowerAdapter adapter = new ListFlowerAdapter(randomFlowers);
-        rvFlowers.setAdapter(adapter);
-        rvFlowers.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        startImageSwitching();
 
         return view;
+    }
+
+    private void startImageSwitching() {
+        handler = new Handler();
+        imageSwitcher = new Runnable() {
+            @Override
+            public void run() {
+                int randomIndex = new Random().nextInt(flowerList.size());
+                Flower randomFlower = flowerList.get(randomIndex);
+
+                imgRecommendation.setImageResource(randomFlower.getPhoto());
+                tvFlowerName.setText(randomFlower.getName());
+                tvMeaning.setText(randomFlower.getMeaning());
+                handler.postDelayed(this, 3000);
+            }
+        };
+
+        handler.post(imageSwitcher);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (handler != null && imageSwitcher != null) {
+            handler.removeCallbacks(imageSwitcher);
+        }
     }
 }
